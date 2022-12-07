3b6c487a251aabf888ae8ab135f88440e4c4169d
Nicholas Guarracino <nicholas.guarracino@ipc.com>
2022-12-07 11:27:50 -0500

    day05

---
+++ day05/src/part2.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let lines = Vec::from_iter(include_str!("../input.txt").lines());
+    let position = lines.iter().position(|line| line.is_empty()).unwrap();
+    let (stacks_input, moves_input) = lines.split_at(position);
+
+    let mut stacks: Vec<Vec<String>> = stacks_input
+        .iter()
+        .map(|stack_input| Vec::from_iter(stack_input.split(' ').map(String::from)))
+        .collect();
+
+    let moves: Vec<Vec<String>> = moves_input
+        .iter()
+        .skip(1)
+        .map(|move_input| Vec::from_iter(move_input.split(' ').map(String::from)))
+        .collect();
+
+    for m in moves {
+        let count = m[1].parse::<usize>().unwrap();
+        let from = m[3].parse::<usize>().unwrap() - 1;
+        let to = m[5].parse::<usize>().unwrap() - 1;
+
+        let mut popped: Vec<String> = Vec::new();
+        for _ in 0..count {
+            popped.push(stacks[from].pop().unwrap())
+        }
+        stacks[to].append(&mut Vec::from_iter(popped.into_iter().rev()));
+    }
+
+    for stack in stacks {
+        print!("{}", stack.last().unwrap());
+    }
+}
